04a15a0453bac6231fe0d3a77a9eb235c0f9aca3
Thomas Clark <tnc1997@virginmedia.com>
2019-06-28 20:08:28 +0100

    build: install gulp core build jest package

--- libraries/array/gulpfile.js
+++ libraries/array/gulpfile.js
@@ -1,8 +1,9 @@
 "use strict";
 
 const {apiDocumenter} = require("@gulp-utils/gulp-core-build-api-documenter");
+const {jest} = require("@gulp-utils/gulp-core-build-jest");
 const {rollup} = require("@gulp-utils/gulp-core-build-rollup");
-const {initialize, jest, parallel, serial, task} = require("@microsoft/gulp-core-build");
+const {initialize, parallel, serial, task} = require("@microsoft/gulp-core-build");
 const {tscCmd, tslintCmd, apiExtractor} = require("@microsoft/gulp-core-build-typescript");
 
 task("default", serial(parallel(tscCmd, tslintCmd), apiExtractor, apiDocumenter, rollup, jest));
